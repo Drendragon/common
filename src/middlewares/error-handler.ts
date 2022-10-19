783d1a594393582a7911fd5d5ea45114609171fd
Németh Sándor <n.sandor93@gmail.com>
2022-10-19 19:22:45 +0200

    updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -12,7 +12,7 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
                 }
             );
 
-    console.log(err);
+    console.error(err);
 
     res.status(400).send({
         errors: [{
